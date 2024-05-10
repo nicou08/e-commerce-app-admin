212df8f315e958e1da50e3f481f90f230579bca9
Nicolas Urrego Guasca <nicolas.guasca16@gmail.com>
2024-05-10 01:20:34 -0600

    Added middleware for authenticaiton

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,22 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
-export default function Home() {
-  function timesTwo(num: number): number {
-    return num * 2;
-  }
-
+export default function SetUpPage() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
       <h1>This is the Admin DashBoard</h1>
       <Button>Click me</Button>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -15,25 +24,8 @@ export default function Home() {
           Get started by editingggg&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
       </div>
+      <h1>THis is a protected route</h1>
     </main>
   );
 }
